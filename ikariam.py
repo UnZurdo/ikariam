8737375285d772aed82695567bace1c30c9eed31
Alberto a <albertoynono@gmail.com>
2017-12-12 01:44:47 +0100

    first

--- ikariam.py
+++ ikariam.py
@@ -1,5 +1,6 @@
 #-- WEB SCRAPING --
 import selenium
+import time
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -15,11 +16,17 @@ driver.get('https://es.ikariam.gameforge.com/')
 driver.find_element_by_id("btn-login").click()
 driver.implicitly_wait(30)
 
+time.sleep(4)
+# s3= Select(driver.find_element_by_id('logServer'))
+#
+# s3.select_by_visible_text('Kaikias')
+driver.implicitly_wait(30)
+
 username = driver.find_element_by_id("loginName")
 password = driver.find_element_by_id("loginPassword")
 
-username.send_keys("user")
-password.send_keys("pw")
+username.send_keys("albertoynono@yahoo.es")
+password.send_keys("juliocesar")
 
 driver.find_element_by_id("loginBtn").click()
 for i in range(10):
